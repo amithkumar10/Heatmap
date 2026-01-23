9494de424ee7d050a92d49270191bf07f7927c74
Amithkumar P Radhakrishnan <amith10mnr@gmail.com>
2026-01-23 10:40:21 +0530

    Browser JS

--- JS Practice/Browser.js
+++ JS Practice/Browser.js
@@ -1,1 +1,33 @@
-console.log("Hello, Browser!");
+//Local Storgage Example
+
+localStorage.setItem("Naam", "Amith"); //Setting item in local storage  
+
+console.log(localStorage.getItem("Naam")); //Getting item from local storage
+
+localStorage.removeItem("Naam"); //Removing item from local storage
+console.log(localStorage.getItem("Naam")); //Now it will return null as the item is removed
+
+localStorage.clear(); //Clearing all items from local storage
+
+
+
+//Session Storage Example
+
+sessionStorage.setItem("Session", "Active"); //Setting item in session storage
+console.log(sessionStorage.getItem("Session")) //Getting item from session storage
+
+sessionStorage.removeItem("Session"); //Removing item from session storage
+console.log(sessionStorage.getItem("Session")); //Now it will return null as the item is removed
+
+sessionStorage.clear(); //Clearing all items from session storage
+
+
+
+
+//Cookies Example
+
+document.cookie = "username=Amith; expires=Fri, 31 Dec 2027 23:59:59 UTC; path=/";
+console.log("COOKIEE: "+document.cookie); //Getting all cookies
+
+document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
+console.log("cookie:"+document.cookie); //Removing all cookies
