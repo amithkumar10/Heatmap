1996dda64a4ab1ec3558b0c1fae911ec3a02ba1b
Amithkumar P Radhakrishnan <amith10mnr@gmail.com>
2026-02-14 10:31:08 +0530

    JS first commit

--- JS Practice/HigherOrderArrayMethods.js
+++ JS Practice/HigherOrderArrayMethods.js
@@ -11,7 +11,7 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
 
 
-//Convet to JSON
+//Convert to JSON
 
 const companiesJSON =  JSON.stringify(Companies);
 console.log(companiesJSON);
